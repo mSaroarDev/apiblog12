7ad5f2c64cff8b054b9c44dc4d775a348632c5a3
Muhammad Saroar <136387545+mSaroarDev@users.noreply.github.com>
2023-08-22 13:16:52 +0600

    code updated

---
+++ src/app/Homepage/page.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div>
+      <div>This is Homepage</div>
+    </div>
+  );
+};
+
+export default Homepage;
---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <>
+      <div className="bg-gray-100 py-5">
+        <main>
+          <div className="flex gap-5 items-center justify-between">
+            <div>
+              <Link href="/">
+                <h1 className="text-base">Simple Blog</h1>
+              </Link>
+            </div>
+            <div>
+              <ul className="flex gap-4 ">
+                <li className="hover:underline duration-300">
+                  <Link href="/">Home</Link>
+                </li>
+                <li className="hover:underline duration-300">
+                  <Link href="/about">About</Link>
+                </li>
+                <li className="hover:underline duration-300">
+                  <Link href="/blog">Blog</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </main>
+      </div>
+    </>
+  );
+}
